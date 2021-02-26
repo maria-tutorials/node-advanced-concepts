ecf1eea26b5b85da7b7c0f66445a2e0e99ba097b
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2021-02-26 12:49:27 +0000

    30. Need More Children!

--- Section 2: Enhancing Node Performance/index.js
+++ Section 2: Enhancing Node Performance/index.js
@@ -7,6 +7,11 @@ console.log(cluster.isMaster);
 
 if (cluster.isMaster) {
     cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
 } else {
     const express = require('express');
     const app = express();
@@ -30,4 +35,8 @@ if (cluster.isMaster) {
 }
 
 // Benchmarking using ab command (apache benchmark), similar to artilery
-  // ab -c 50 -n 500 localhost:300/fast
+  // ab -c 50 -n 500 localhost:3000/fast
+
+  // ab -c 1 -n 1 localhost:3000/
+  // ab -c 2 -n 2 localhost:3000/
+  // ab -c 6 -n 6 localhost:3000/
