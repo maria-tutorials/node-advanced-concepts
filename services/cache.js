bb7f8e2bb70f37afa23472a4bbdcf7ad55c47d47
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2021-02-27 22:38:56 +0000

    58. Key Creation 59. Restoring Redis Config 60. Cache Implementation 61. Resolving Values

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const url = 'redis://127.0.0.1:6379';
+const client = redis.createClient(url);
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-    console.log('running a query');
+mongoose.Query.prototype.exec = async function () {
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    }));
 
-    return exec.apply(this, arguments);
+    const cached_val =  await client.get(key);
+    if (cached_val) {
+        const doc = JSON.parse(cached_val);
+
+        return Array.isArray(doc) 
+        ? doc.map( d => new this.model(d))
+        : new this.model(doc);
+    }
+
+    const result = await exec.apply(this, arguments);
+
+    client.set(key, JSON.stringify(result));
+
+    return result;
 }
